9ef23c2124c57f330d90cc2684409a25e19c298f
bryanmsombra27 <bryanjm96@gmail.com>
2025-03-07 16:29:28 -0600

    adding jwt module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '10h' },
+    }),
+  ],
   controllers: [],
   providers: [],
 })
