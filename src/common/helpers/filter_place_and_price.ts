70c1838afb549e1bbb80c37f96b7cc7bb5f31f15
bryanmsombra27 <bryanjm96@gmail.com>
2025-04-30 15:26:39 -0600

    refactor finished

--- src/common/helpers/filter_place_and_price.ts
+++ src/common/helpers/filter_place_and_price.ts
@@ -1,3 +1,5 @@
+import * as _ from 'lodash';
+
 /**
  *
  * @param p Es el archivo utilizado para leer el XML
@@ -5,197 +7,65 @@
  */
 const lugaresIds = [
   6564, 11028, 2869, 7347, 10500, 26774, 5799, 14500, 8241, 3014, 20864, 9991,
-  9317, 14432, 9476, 9936, 6788, 6797, 5460, 8396, 7179, 24079, 6594, 10843,
-  10704, 2773, 2972, 8197, 15659, 6490, 6670, 6718, 8740, 8438, 8831, 6847,
-  26329, 5961, 24272, 9488, 9829, 9862, 7144, 6643, 9539, 13411, 2876, 3305,
-  13090, 26416, 5358, 2669, 2798, 22451, 6820, 8504, 25805, 6438, 3157, 13089,
-  10145, 22150, 3447, 3494, 9763, 12972, 8499, 22482, 25679, 9367, 5020, 5773,
-  7278, 21536, 6559, 6787, 6260, 6739, 6757, 7357, 6241, 15684, 7888,
+  9317, 14432, 9476, 9936, 6788, 6797,
+  // 5460,
+  8396, 7179, 24079, 6594, 10843, 10704, 2773, 2972, 8197, 15659, 6490, 6670,
+  6718, 8740, 8438, 8831, 6847, 26329, 5961, 24272, 9488, 9829, 9862, 7144,
+  6643, 9539, 13411, 2876, 3305, 13090, 26416, 5358, 2669, 2798, 22451, 6820,
+  8504, 25805, 6438, 3157, 13089, 10145, 22150, 3447, 3494, 9763, 12972, 8499,
+  22482, 25679, 9367, 5020, 5773, 7278, 21536, 6559, 6787, 6260, 6739, 6757,
+  7357, 6241, 15684, 7888,
 ];
 
 const arregloFiltradoPorLugarAcapulco = (p) => {
-  let arreglo = [];
-  return (arreglo = p.places.place.filter(
-    (place) =>
-      place['@_place_id'] == 6564 ||
-      place['@_place_id'] == 11028 ||
-      place['@_place_id'] == 2869 ||
-      place['@_place_id'] == 7347 ||
-      place['@_place_id'] == 10500 ||
-      place['@_place_id'] == 26774 ||
-      place['@_place_id'] == 5799 ||
-      place['@_place_id'] == 14500 ||
-      place['@_place_id'] == 8241 ||
-      place['@_place_id'] == 3014 ||
-      place['@_place_id'] == 20864 ||
-      place['@_place_id'] == 9991 ||
-      place['@_place_id'] == 9317 ||
-      place['@_place_id'] == 14432 ||
-      place['@_place_id'] == 9476 ||
-      place['@_place_id'] == 9936 ||
-      place['@_place_id'] == 6788 ||
-      place['@_place_id'] == 6797 ||
-      place['@_place_id'] == 5460 ||
-      place['@_place_id'] == 8396 ||
-      place['@_place_id'] == 7179 ||
-      place['@_place_id'] == 24079 ||
-      place['@_place_id'] == 6594 ||
-      place['@_place_id'] == 10843 ||
-      place['@_place_id'] == 10704 ||
-      place['@_place_id'] == 2773 ||
-      place['@_place_id'] == 2972 ||
-      place['@_place_id'] == 8197 ||
-      place['@_place_id'] == 15659 ||
-      place['@_place_id'] == 6490 ||
-      place['@_place_id'] == 6670 ||
-      place['@_place_id'] == 6718 ||
-      place['@_place_id'] == 8740 ||
-      place['@_place_id'] == 8438 ||
-      place['@_place_id'] == 8831 ||
-      place['@_place_id'] == 6847 ||
-      place['@_place_id'] == 26329 ||
-      place['@_place_id'] == 5961 ||
-      place['@_place_id'] == 24272 ||
-      place['@_place_id'] == 9488 ||
-      place['@_place_id'] == 9829 ||
-      place['@_place_id'] == 9862 ||
-      place['@_place_id'] == 7144 ||
-      place['@_place_id'] == 6643 ||
-      place['@_place_id'] == 9539 ||
-      place['@_place_id'] == 13411 ||
-      place['@_place_id'] == 2876 ||
-      place['@_place_id'] == 3305 ||
-      place['@_place_id'] == 13090 ||
-      place['@_place_id'] == 26416 ||
-      place['@_place_id'] == 5358 ||
-      place['@_place_id'] == 2669 ||
-      place['@_place_id'] == 2798 ||
-      place['@_place_id'] == 22451 ||
-      place['@_place_id'] == 6820 ||
-      place['@_place_id'] == 8504 ||
-      place['@_place_id'] == 25805 ||
-      place['@_place_id'] == 6438 ||
-      place['@_place_id'] == 3157 ||
-      place['@_place_id'] == 13089 ||
-      place['@_place_id'] == 10145 ||
-      place['@_place_id'] == 22150 ||
-      place['@_place_id'] == 3447 ||
-      place['@_place_id'] == 3494 ||
-      place['@_place_id'] == 9763 ||
-      place['@_place_id'] == 12972 ||
-      place['@_place_id'] == 8499 ||
-      place['@_place_id'] == 22482 ||
-      place['@_place_id'] == 25679 ||
-      place['@_place_id'] == 9367 ||
-      place['@_place_id'] == 5020 ||
-      place['@_place_id'] == 5773 ||
-      place['@_place_id'] == 7278 ||
-      place['@_place_id'] == 21536 ||
-      place['@_place_id'] == 6559 ||
-      place['@_place_id'] == 6787 ||
-      place['@_place_id'] == 6260 ||
-      place['@_place_id'] == 6739 ||
-      place['@_place_id'] == 6757 ||
-      place['@_place_id'] == 7357 ||
-      place['@_place_id'] == 6241 ||
-      place['@_place_id'] == 15684 ||
-      place['@_place_id'] == 7888,
-  ));
-};
-/**
- *
- * @param p Es el archivo utilizado para leer el XML
- * @returns En base al archivo utilizado, retorna un arreglo de Precios.
- */
-const arregloFiltradoPorPrecioAcapulco = (p) => {
-  let arreglo = [];
+  // console.log(
+  //   p.places.place.find((e) => e['@_place_id'] == 26774),
+  //   'GASOLINERA  XML ?====',
+  // );
 
-  return (arreglo = p.places.place.filter(
-    (precio) =>
-      precio['@_place_id'] == 6564 ||
-      precio['@_place_id'] == 11028 ||
-      precio['@_place_id'] == 2869 ||
-      precio['@_place_id'] == 7347 ||
-      precio['@_place_id'] == 10500 ||
-      precio['@_place_id'] == 26774 ||
-      precio['@_place_id'] == 5799 ||
-      precio['@_place_id'] == 14500 ||
-      precio['@_place_id'] == 8241 ||
-      precio['@_place_id'] == 3014 ||
-      precio['@_place_id'] == 20864 ||
-      precio['@_place_id'] == 9991 ||
-      precio['@_place_id'] == 9317 ||
-      precio['@_place_id'] == 14432 ||
-      precio['@_place_id'] == 9476 ||
-      precio['@_place_id'] == 9936 ||
-      precio['@_place_id'] == 6788 ||
-      precio['@_place_id'] == 6797 ||
-      precio['@_place_id'] == 5460 ||
-      precio['@_place_id'] == 8396 ||
-      precio['@_place_id'] == 7179 ||
-      precio['@_place_id'] == 24079 ||
-      precio['@_place_id'] == 6594 ||
-      precio['@_place_id'] == 10843 ||
-      precio['@_place_id'] == 10704 ||
-      precio['@_place_id'] == 2773 ||
-      precio['@_place_id'] == 2972 ||
-      precio['@_place_id'] == 8197 ||
-      precio['@_place_id'] == 15659 ||
-      precio['@_place_id'] == 6490 ||
-      precio['@_place_id'] == 6670 ||
-      precio['@_place_id'] == 6718 ||
-      precio['@_place_id'] == 8740 ||
-      precio['@_place_id'] == 8438 ||
-      precio['@_place_id'] == 8831 ||
-      precio['@_place_id'] == 6847 ||
-      precio['@_place_id'] == 26329 ||
-      precio['@_place_id'] == 5961 ||
-      precio['@_place_id'] == 24272 ||
-      precio['@_place_id'] == 9488 ||
-      precio['@_place_id'] == 9829 ||
-      precio['@_place_id'] == 9862 ||
-      precio['@_place_id'] == 7144 ||
-      precio['@_place_id'] == 6643 ||
-      precio['@_place_id'] == 9539 ||
-      precio['@_place_id'] == 13411 ||
-      precio['@_place_id'] == 2876 ||
-      precio['@_place_id'] == 3305 ||
-      precio['@_place_id'] == 13090 ||
-      precio['@_place_id'] == 26416 ||
-      precio['@_place_id'] == 5358 ||
-      precio['@_place_id'] == 2669 ||
-      precio['@_place_id'] == 2798 ||
-      precio['@_place_id'] == 22451 ||
-      precio['@_place_id'] == 6820 ||
-      precio['@_place_id'] == 8504 ||
-      precio['@_place_id'] == 25805 ||
-      precio['@_place_id'] == 6438 ||
-      precio['@_place_id'] == 3157 ||
-      precio['@_place_id'] == 13089 ||
-      precio['@_place_id'] == 10145 ||
-      precio['@_place_id'] == 22150 ||
-      precio['@_place_id'] == 3447 ||
-      precio['@_place_id'] == 3494 ||
-      precio['@_place_id'] == 9763 ||
-      precio['@_place_id'] == 12972 ||
-      precio['@_place_id'] == 8499 ||
-      precio['@_place_id'] == 22482 ||
-      precio['@_place_id'] == 25679 ||
-      precio['@_place_id'] == 9367 ||
-      precio['@_place_id'] == 5020 ||
-      precio['@_place_id'] == 5773 ||
-      precio['@_place_id'] == 7278 ||
-      precio['@_place_id'] == 21536 ||
-      precio['@_place_id'] == 6559 ||
-      precio['@_place_id'] == 6787 ||
-      precio['@_place_id'] == 6260 ||
-      precio['@_place_id'] == 6739 ||
-      precio['@_place_id'] == 6757 ||
-      precio['@_place_id'] == 7357 ||
-      precio['@_place_id'] == 6241 ||
-      precio['@_place_id'] == 15684 ||
-      precio['@_place_id'] == 7888,
-  ));
+  return p.places.place.filter((place) =>
+    lugaresIds.includes(place['@_place_id']),
+  );
 };
 
-export { arregloFiltradoPorLugarAcapulco, arregloFiltradoPorPrecioAcapulco };
+const unionDeArregloLugarYPrecio = (lugar, precio) => {
+  let mergedArrays = [];
+  const groupByPlaceIdPricing = _.groupBy(precio, '@_place_id');
+
+  for (const item in groupByPlaceIdPricing) {
+    const placeFound = lugar.find((e) => e['@_place_id'] == item);
+    const priceFound = groupByPlaceIdPricing[item];
+    // console.log(priceFound, 'precios de gasolina =======');
+
+    if (placeFound && priceFound) {
+      let priceFormatObject: any = {};
+
+      priceFound.forEach((price) => {
+        if (price.gas_price.length > 0) {
+          price.gas_price.forEach((gasPrice) => {
+            priceFormatObject.type = {
+              ...priceFormatObject.type,
+              [gasPrice['@_type']]: gasPrice['#text'],
+            };
+          });
+        } else {
+          priceFormatObject.type = {
+            ...priceFormatObject.type,
+            [price['gas_price']['@_type']]: price['gas_price']['#text'],
+          };
+        }
+      });
+
+      mergedArrays.push({
+        ...placeFound,
+        ...priceFormatObject,
+      });
+    }
+  }
+
+  console.log(mergedArrays, 'MERGED ARRAYS');
+
+  return mergedArrays;
+};
+
+export { arregloFiltradoPorLugarAcapulco, unionDeArregloLugarYPrecio };
--- src/zones/zones.service.ts
+++ src/zones/zones.service.ts
@@ -10,7 +10,10 @@ import * as path from 'path';
 import { XMLParser } from 'fast-xml-parser';
 import { reporteXML } from 'src/common/helpers/writeFileXML';
 import { arregloFinal } from 'src/common/helpers/registroZona';
-import { arregloFiltradoPorLugarAcapulco } from 'src/common/helpers/filter_place_and_price';
+import {
+  arregloFiltradoPorLugarAcapulco,
+  unionDeArregloLugarYPrecio,
+} from 'src/common/helpers/filter_place_and_price';
 const options = {
   ignoreAttributes: false,
   attributeNamePrefix: '@_',
@@ -48,85 +51,26 @@ export class ZonesService {
       const lugaresXML = parser.parse(data);
       const preciosXML = parser.parse(data2);
 
-      console.log(lugaresXML.places.place, 'LUGARES XML ');
-      console.log(preciosXML.places.place, 'PRECIOS XML ');
-
       const lugar = arregloFiltradoPorLugarAcapulco(lugaresXML);
       const precio = arregloFiltradoPorLugarAcapulco(preciosXML);
 
-      const precioyLugar = precio.filter((precio) => {
-        const Gasolinera = lugar.find(
-          (lugar) => lugar['@_place_id'] == precio['@_place_id'],
-        );
-        precio.lugar = Gasolinera;
-
-        return precio;
-      });
-
-      const arregloDefinitivo = precioyLugar.filter((precio) => {
-        if (
-          precio['gas_price'][0] &&
-          precio['gas_price'][1] &&
-          precio['gas_price'][2]
-        ) {
-          precio.type = {
-            [precio['gas_price'][0]['@_type']]: {
-              name: precio['gas_price'][0]['@_type'],
-              price: precio['gas_price'][0]['#text'],
-            },
-            [precio['gas_price'][1]['@_type']]: {
-              name: precio['gas_price'][1]['@_type'],
-              price: precio['gas_price'][1]['#text'],
-            },
-            [precio['gas_price'][2]['@_type']]: {
-              name: precio['gas_price'][2]['@_type'],
-              price: precio['gas_price'][2]['#text'],
-            },
-          };
-        } else if (precio['gas_price'][0] && precio['gas_price'][1]) {
-          precio.type = {
-            [precio['gas_price'][0]['@_type']]: {
-              name: precio['gas_price'][0]['@_type'],
-              price: precio['gas_price'][0]['#text'],
-            },
-            [precio['gas_price'][1]['@_type']]: {
-              name: precio['gas_price'][1]['@_type'],
-              price: precio['gas_price'][1]['#text'],
-            },
-          };
-        } else if (precio['gas_price'][0]) {
-          precio.type = {
-            [precio['gas_price'][0]['@_type']]: {
-              name: precio['gas_price'][0]['@_type'],
-              price: precio['gas_price'][0]['#text'],
-            },
-          };
-        } else {
-          precio.type = {
-            [precio['gas_price']['@_type']]: {
-              name: precio['gas_price']['@_type'],
-              price: precio['gas_price']['#text'],
-            },
-          };
-        }
-
-        return precio;
-      });
+      const arregloDefinitivo = unionDeArregloLugarYPrecio(lugar, precio);
 
       switch (zona) {
         //registro repetido original 6
         case 'magallanes':
           registrosEnUnaSolaFila = arregloDefinitivo.filter(
             (gasolinera) =>
-              gasolinera.lugar.cre_id.split('/')[1] == 4337 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 3081 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 1162 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 3901 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 9356 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 23041,
+              gasolinera.cre_id.split('/')[1] == 4337 ||
+              gasolinera.cre_id.split('/')[1] == 3081 ||
+              gasolinera.cre_id.split('/')[1] == 1162 ||
+              gasolinera.cre_id.split('/')[1] == 3901 ||
+              gasolinera.cre_id.split('/')[1] == 9356 ||
+              gasolinera.cre_id.split('/')[1] == 23041,
           );
 
-          arregloFiltradoPorZona = arregloFinal(registrosEnUnaSolaFila);
+          //  arregloFiltradoPorZona = registrosEnUnaSolaFila;
+          arregloFiltradoPorZona = registrosEnUnaSolaFila;
 
           break;
 
@@ -134,14 +78,14 @@ export class ZonesService {
         case 'coyuca':
           registrosEnUnaSolaFila = arregloDefinitivo.filter(
             (gasolinera) =>
-              gasolinera.lugar.cre_id.split('/')[1] == 4263 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 12908 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 6980 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 1180 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 13644,
+              gasolinera.cre_id.split('/')[1] == 4263 ||
+              gasolinera.cre_id.split('/')[1] == 12908 ||
+              gasolinera.cre_id.split('/')[1] == 6980 ||
+              gasolinera.cre_id.split('/')[1] == 1180 ||
+              gasolinera.cre_id.split('/')[1] == 13644,
           );
 
-          arregloFiltradoPorZona = arregloFinal(registrosEnUnaSolaFila);
+          arregloFiltradoPorZona = registrosEnUnaSolaFila;
 
           break;
 
@@ -149,16 +93,16 @@ export class ZonesService {
         case 'diamante':
           registrosEnUnaSolaFila = arregloDefinitivo.filter(
             (gasolinera) =>
-              gasolinera.lugar.cre_id.split('/')[1] == 9012 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 8699 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 12837 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 8664 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 9016 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 4328 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 4352,
+              gasolinera.cre_id.split('/')[1] == 9012 ||
+              gasolinera.cre_id.split('/')[1] == 8699 ||
+              gasolinera.cre_id.split('/')[1] == 12837 ||
+              gasolinera.cre_id.split('/')[1] == 8664 ||
+              gasolinera.cre_id.split('/')[1] == 9016 ||
+              gasolinera.cre_id.split('/')[1] == 4328 ||
+              gasolinera.cre_id.split('/')[1] == 4352,
           );
 
-          arregloFiltradoPorZona = arregloFinal(registrosEnUnaSolaFila);
+          arregloFiltradoPorZona = registrosEnUnaSolaFila;
 
           break;
 
@@ -166,17 +110,17 @@ export class ZonesService {
         case 'llano':
           registrosEnUnaSolaFila = arregloDefinitivo.filter(
             (gasolinera) =>
-              gasolinera.lugar.cre_id.split('/')[1] == 4289 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 5985 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 6888 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 4765 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 20920 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 22162,
+              gasolinera.cre_id.split('/')[1] == 4289 ||
+              gasolinera.cre_id.split('/')[1] == 5985 ||
+              gasolinera.cre_id.split('/')[1] == 6888 ||
+              gasolinera.cre_id.split('/')[1] == 4765 ||
+              gasolinera.cre_id.split('/')[1] == 20920 ||
+              gasolinera.cre_id.split('/')[1] == 22162,
           );
 
           //logica de un solo registro cuando se repite 2 veces
           //===============================================================
-          arregloFiltradoPorZona = arregloFinal(registrosEnUnaSolaFila);
+          arregloFiltradoPorZona = registrosEnUnaSolaFila;
           //==========================================================
           break;
 
@@ -184,22 +128,22 @@ export class ZonesService {
         case 'chilpo':
           registrosEnUnaSolaFila = arregloDefinitivo.filter(
             (gasolinera) =>
-              gasolinera.lugar.cre_id.split('/')[1] == 19736 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 1633 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 1638 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 8229 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 10726 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 6887 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 20003 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 22049 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 8327 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 6059 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 4349 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 4377 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 19221,
+              gasolinera.cre_id.split('/')[1] == 19736 ||
+              gasolinera.cre_id.split('/')[1] == 1633 ||
+              gasolinera.cre_id.split('/')[1] == 1638 ||
+              gasolinera.cre_id.split('/')[1] == 8229 ||
+              gasolinera.cre_id.split('/')[1] == 10726 ||
+              gasolinera.cre_id.split('/')[1] == 6887 ||
+              gasolinera.cre_id.split('/')[1] == 20003 ||
+              gasolinera.cre_id.split('/')[1] == 22049 ||
+              gasolinera.cre_id.split('/')[1] == 8327 ||
+              gasolinera.cre_id.split('/')[1] == 6059 ||
+              gasolinera.cre_id.split('/')[1] == 4349 ||
+              gasolinera.cre_id.split('/')[1] == 4377 ||
+              gasolinera.cre_id.split('/')[1] == 19221,
           );
 
-          arregloFiltradoPorZona = arregloFinal(registrosEnUnaSolaFila);
+          arregloFiltradoPorZona = registrosEnUnaSolaFila;
 
           break;
 
@@ -207,46 +151,46 @@ export class ZonesService {
         case 'rena':
           registrosEnUnaSolaFila = arregloDefinitivo.filter(
             (gasolinera) =>
-              gasolinera.lugar.cre_id.split('/')[1] == 4332 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 2942 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 9588 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 1192 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 1190 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 4326 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 4323 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 4351,
+              gasolinera.cre_id.split('/')[1] == 4332 ||
+              gasolinera.cre_id.split('/')[1] == 2942 ||
+              gasolinera.cre_id.split('/')[1] == 9588 ||
+              gasolinera.cre_id.split('/')[1] == 1192 ||
+              gasolinera.cre_id.split('/')[1] == 1190 ||
+              gasolinera.cre_id.split('/')[1] == 4326 ||
+              gasolinera.cre_id.split('/')[1] == 4323 ||
+              gasolinera.cre_id.split('/')[1] == 4351,
           );
 
-          arregloFiltradoPorZona = arregloFinal(registrosEnUnaSolaFila);
+          arregloFiltradoPorZona = registrosEnUnaSolaFila;
 
           break;
         //original 3
         case 'muller':
           registrosEnUnaSolaFila = arregloDefinitivo.filter(
             (gasolinera) =>
-              gasolinera.lugar.cre_id.split('/')[1] == 6409 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 13058 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 4287,
+              gasolinera.cre_id.split('/')[1] == 6409 ||
+              gasolinera.cre_id.split('/')[1] == 13058 ||
+              gasolinera.cre_id.split('/')[1] == 4287,
           );
-          arregloFiltradoPorZona = arregloFinal(registrosEnUnaSolaFila);
+          arregloFiltradoPorZona = registrosEnUnaSolaFila;
 
           break;
         //registro repetido  original 9
         case 'servifer':
           registrosEnUnaSolaFila = arregloDefinitivo.filter(
             (gasolinera) =>
-              gasolinera.lugar.cre_id.split('/')[1] == 4317 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 4770 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 3644 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 7528 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 6832 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 7793 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 4311 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 4293 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 13086,
+              gasolinera.cre_id.split('/')[1] == 4317 ||
+              gasolinera.cre_id.split('/')[1] == 4770 ||
+              gasolinera.cre_id.split('/')[1] == 3644 ||
+              gasolinera.cre_id.split('/')[1] == 7528 ||
+              gasolinera.cre_id.split('/')[1] == 6832 ||
+              gasolinera.cre_id.split('/')[1] == 7793 ||
+              gasolinera.cre_id.split('/')[1] == 4311 ||
+              gasolinera.cre_id.split('/')[1] == 4293 ||
+              gasolinera.cre_id.split('/')[1] == 13086,
           );
 
-          arregloFiltradoPorZona = arregloFinal(registrosEnUnaSolaFila);
+          arregloFiltradoPorZona = registrosEnUnaSolaFila;
 
           break;
 
@@ -254,18 +198,18 @@ export class ZonesService {
         case 'cruz-grande':
           registrosEnUnaSolaFila = arregloDefinitivo.filter(
             (gasolinera) =>
-              gasolinera.lugar.cre_id.split('/')[1] == 22575 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 4341 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 21109 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 8438 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 8441 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 8424 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 4577 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 4574 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 8420,
+              gasolinera.cre_id.split('/')[1] == 22575 ||
+              gasolinera.cre_id.split('/')[1] == 4341 ||
+              gasolinera.cre_id.split('/')[1] == 21109 ||
+              gasolinera.cre_id.split('/')[1] == 8438 ||
+              gasolinera.cre_id.split('/')[1] == 8441 ||
+              gasolinera.cre_id.split('/')[1] == 8424 ||
+              gasolinera.cre_id.split('/')[1] == 4577 ||
+              gasolinera.cre_id.split('/')[1] == 4574 ||
+              gasolinera.cre_id.split('/')[1] == 8420,
           );
 
-          arregloFiltradoPorZona = arregloFinal(registrosEnUnaSolaFila);
+          arregloFiltradoPorZona = registrosEnUnaSolaFila;
 
           break;
 
@@ -273,20 +217,20 @@ export class ZonesService {
         case 'ejido-modelo':
           registrosEnUnaSolaFila = arregloDefinitivo.filter(
             (gasolinera) =>
-              gasolinera.lugar.cre_id.split('/')[1] == 11270 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 1206 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 1535 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 10951 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 22646 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 5537 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 1453 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 1087 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 4334 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 6355 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 6285,
+              gasolinera.cre_id.split('/')[1] == 11270 ||
+              gasolinera.cre_id.split('/')[1] == 1206 ||
+              gasolinera.cre_id.split('/')[1] == 1535 ||
+              gasolinera.cre_id.split('/')[1] == 10951 ||
+              gasolinera.cre_id.split('/')[1] == 22646 ||
+              gasolinera.cre_id.split('/')[1] == 5537 ||
+              gasolinera.cre_id.split('/')[1] == 1453 ||
+              gasolinera.cre_id.split('/')[1] == 1087 ||
+              gasolinera.cre_id.split('/')[1] == 4334 ||
+              gasolinera.cre_id.split('/')[1] == 6355 ||
+              gasolinera.cre_id.split('/')[1] == 6285,
           );
 
-          arregloFiltradoPorZona = arregloFinal(registrosEnUnaSolaFila);
+          arregloFiltradoPorZona = registrosEnUnaSolaFila;
 
           break;
 
@@ -294,15 +238,15 @@ export class ZonesService {
         case 'costera-diana':
           registrosEnUnaSolaFila = arregloDefinitivo.filter(
             (gasolinera) =>
-              gasolinera.lugar.cre_id.split('/')[1] == 4339 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 1151 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 10950 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 9019 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 19967 ||
-              gasolinera.lugar.cre_id.split('/')[1] == 4353,
+              gasolinera.cre_id.split('/')[1] == 4339 ||
+              gasolinera.cre_id.split('/')[1] == 1151 ||
+              gasolinera.cre_id.split('/')[1] == 10950 ||
+              gasolinera.cre_id.split('/')[1] == 9019 ||
+              gasolinera.cre_id.split('/')[1] == 19967 ||
+              gasolinera.cre_id.split('/')[1] == 4353,
           );
 
-          arregloFiltradoPorZona = arregloFinal(registrosEnUnaSolaFila);
+          arregloFiltradoPorZona = registrosEnUnaSolaFila;
 
           break;
 
