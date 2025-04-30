1c22ecc05e39460568210791de518da29b6ce594
bryanmsombra27 <bryanjm96@gmail.com>
2025-04-30 15:30:19 -0600

    adding download service when a server is being reset

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import * as cron from 'node-cron';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // reporteXML('prices', 'places');
+  reporteXML('prices', 'places');
 
   cron.schedule('0 */4 * * *', () => {
     reporteXML('prices', 'places');
